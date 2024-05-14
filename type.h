808774b4f1f71b41e8066c88de8707168eec2808
oplushappy <91440425+oplushappy@users.noreply.github.com>
2024-05-15 01:43:15 +0800

    Partner (#3)
    * player can choose what type of partner
    
    * add blood bar, deduct blood
    
    * combine player、partner；partner implement shoot

--- type.h
+++ type.h
@@ -2,12 +2,20 @@
 #define TYPES_H
 
 #include "raylib.h"
+#include "raygui.h" 
 
 // 定義敵人波次的枚舉類型
 typedef enum { FIRST = 0, SECOND, THIRD } EnemyWave;
 
 // 玩家結構
-typedef struct Player {
+// typedef struct Player {
+    // Rectangle rec;
+    // Vector2 speed;
+    // Color color;
+// } Player;
+typedef struct Player{
+    int HP;
+    int AttackPower;
     Rectangle rec;
     Vector2 speed;
     Color color;
@@ -28,10 +36,12 @@ typedef struct Enemy{
     Rectangle rec;
     Vector2 speed;
     bool active;
+    // int attackPower;
+    int AttackPower;
     Color color;
     int HP;
-    int Atk;
-    float frequency;
+    // int Atk;
+    // float frequency;
 } Enemy;
 
 // 射擊結構
@@ -42,16 +52,47 @@ typedef struct Shoot {
     Color color;
 } Shoot;
 
+//////////////////////////////////
 // 夥伴結構
+// typedef struct Partner {
+    // Rectangle rec;
+    // int health;
+    // int attackPower;
+    // float attackRate;
+    // bool active;
+    // float attackTimer;
+// } Partner;
+typedef enum {
+    PARTNER_TYPE_ONE,
+    PARTNER_TYPE_TWO,
+    PARTNER_TYPE_THREE
+} PartnerType;
+
 typedef struct Partner {
     Rectangle rec;
-    int health;
-    int attackPower;
+    int HP;
+    int maxHealth;
+    int AttackPower;
     float attackRate;
     bool active;
     float attackTimer;
+    // float hitCooldown;  // 用于受击冷却
+    // float lastHitTime;  // 上次被攻击的时间
+    float shootTimer;  // 跟踪下一次射擊的計時器
+    float lastShootTime;  // 上一次射擊的時間
+    PartnerType type;  // 添加一个类型字段
 } Partner;
 
+// typedef struct Shoot {
+    // Rectangle rec;
+    // Vector2 speed;
+    // bool active;
+    // Color color;
+// } Shoot;
+
+// static Shoot* partnerBullets;
+///////////////////////////////////////
 // 其他共享結構和宣告 ...
+
 
 #endif // TYPES_H
